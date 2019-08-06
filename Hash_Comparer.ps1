98978a2bbbca7c23d93767790c2077ce7399c964
Akvile <kiskisakvile@gmail.com>
2019-08-06 18:45:23 +0200

    Updated readme and added first script

---
+++ Hash_Comparer.ps1
@@ -0,0 +1,91 @@
+﻿# Hash Comparer Script
+# By: Akvile Kiskis
+# Date: 08/04/2019
+#
+# The purpose of this script is to compare a downloaded file hash to the one listed on their website to ensure integrity.
+
+###### List all functions first ######
+
+# Function to get file name
+Function Get-FileName
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.InitialDirectory = "C:\Users\%USERPROFILE%\Downloads"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.FileName
+ }
+
+# Function to show the hash options
+function Show-Menu
+{
+     param (
+           [string]$Title = 'Hash Selection Menu'
+     )
+     cls
+     Write-Host "================ $Title ================"
+     
+     Write-Host "1: Press '1' for MD5."
+     Write-Host "2: Press '2' for SHA1."
+     Write-Host "3: Press '3' for SHA256."
+     Write-Host "4: Press '4' for SHA384."
+     Write-Host "5: Press '5' for SHA512."
+     Write-Host "6: Press '6' to exit the program."
+}
+
+# Function to compare hashes
+function Compare-Hashes ($Algorithm)
+{
+    $fileHash = Get-FileHash $filePath -Algorithm $Algorithm | Select-Object -ExpandProperty Hash
+    # Prompt user for the listed file hash they received
+    $originalHash = Read-Host -Prompt "Enter the file hash you want to compare this file to: "
+    # Display both
+    Write-Host "The file hash on your system is: $fileHash"
+    Write-Host "What the file hash should be is: $originalHash"
+
+    # compare them
+    if ($fileHash -ne $originalHash)
+    {
+        Write-Warning "The hashes don't match!"
+    }
+    else
+    {
+        Write-Host "The hashes match!" -ForegroundColor Green
+    }
+}
+
+
+###### Begin Script ######
+
+# Prompt user for the file hash they want to check
+$filePath = Get-FileName 
+
+ 
+ # Check the hash - Give them options, let it run on a loop 
+do
+{
+     Show-Menu
+     $input = Read-Host "Please make a selection"
+     switch ($input)
+     {
+           '1' {
+                $Algorithm = "MD5"
+                Compare-Hashes($Algorithm)
+           } '2' {
+                $Algorithm = "SHA1"
+                Compare-Hashes($Algorithm)
+           } '3' {
+                $Algorithm = "SHA256"
+                Compare-Hashes($Algorithm)
+           } '4' {
+                $Algorithm = "SHA384"
+                Compare-Hashes($Algorithm)
+           } '5' {
+                $Algorithm = "SHA512"
+                Compare-Hashes($Algorithm)
+           } 
+
+     }
+    pause
+}
+until ($input -eq '6')
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Scripts
+
+The only way I'll ever practice the scripting I learned in school is if I have a use case for it. This repository is to store all of these use cases.
+
+For now, I just have one so I will list it here. I'm sure as this repository grows over time, there will be some restructuring.
+
+## Hash Comparer - PowerShell
+
+Oftentimes when downloading files or programs off of the Internet, the owners/creators/developers will leave the hash of the file to ensure integrity.
+
+I found it tedious to have to compare this manually, so I wrote a script for it to make the comparison easier. When you run the script, a file dialog box will open for you to pick the file you're checking. Then, you can select what type of hash you want to compare it to and paste in what the hash was on the website you downloaded the file from. The script will compare the two and inform you if the hashes are the same or not. You can run this multiple times on the same file with different hash types.
+
+### Screenshots:
+
+*Test File*
+
+<img src="screenshots/website_hash.png" alt="sample hash"/>
+
+*Valid hash comparison*
+
+<img src="screenshots/example.png" alt="good match"/>
+
+*Invalid hash comparison*
+
+<img src="screenshots/no_match.png" alt="bad match"/>
